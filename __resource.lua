6a5e221829fc121b380fcd3b719305a27e9d6d55
LucaNL <lucanl.beheer@gmail.com>
2022-03-21 08:20:07 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-author 'LucaNL'
+author 'LucaNL#2230'
 description 'A script that allows you to create vehicles for only one or several jobs, so you can make sure people dont steal emergency vehicles. Like police, ambulance and so on. This script causes no performance loss.'
 version '1.0.0'
 
